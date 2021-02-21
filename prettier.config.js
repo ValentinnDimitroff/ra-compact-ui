0860a69a57aae400a94c16bfefd9bea249d300cc
Valentin Dimitroff <26602880+ValentinnDimitroff@users.noreply.github.com>
2021-02-21 20:12:29 +0200

    Add prettier

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: false,
+    singleQuote: true,
+    printWidth: 100,
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    jsxSingleQuote: false,
+    quoteProps: 'as-needed',
+    rangeStart: 0,
+    rangeEnd: Infinity,
+    useTabs: false,
+};
